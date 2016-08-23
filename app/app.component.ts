6d87291326f06ed18ae38396746c97d64d06fab8
zebesta <chris.sebesta@gmail.com>
2016-08-23 14:07:37 -0400

    fixed promises to actually work

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Plant } from './plant'
 import { PlantsComponent } from './plants.component'
+import './rxjs-extensions';
 
 @Component({
   selector: 'my-app',
@@ -8,5 +9,4 @@ import { PlantsComponent } from './plants.component'
 })
 export class AppComponent {
   title = 'Simple Plant App!!!';
-  plant: Plant = {"_id":"57a8ac953200c113b0000002","type":"vegetable","color":"Orange","name":"Carrot","__v":0};
 }
