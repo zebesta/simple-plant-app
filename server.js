088ad6c6509e6a65f0ec146323b1df96ecae6174
zebesta <chris.sebesta@gmail.com>
2016-09-13 11:30:02 -0400

    changed home and removed some old router stuff

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ var bodyParser = require('body-parser');
 // //locally degined schema for mongoose
 // var Plant     = require('./app/models/plant');
 
-app.set('view engine', 'jade');
+// app.set('view engine', 'jade');
 //allow cross origin requests
 app.use(cors());
 //app.options('*', cors()); // include before other routes
@@ -22,18 +22,18 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 var port = process.env.PORT || 8080;        //set port
-var router = express.Router();              // get an instance of the express Router
+// var router = express.Router();              // get an instance of the express Router
 
 //middleware to use for every request sent to the API, regardless of what it is for
-router.use(function (req, res, next){
-  //do logging whne API is accessed
-  console.log('Something is hapenning!');
-  next(); // goes to the next route instead of stopping here
-});
+// router.use(function (req, res, next){
+//   //do logging whne API is accessed
+//   console.log('Something is hapenning!');
+//   next(); // goes to the next route instead of stopping here
+// });
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
-router.get('/', function(req, res) {
-    res.json({ message: 'hooray! welcome to the api!' });
-});
+// router.get('/', function(req, res) {
+//     res.json({ message: 'hooray! welcome to the api!' });
+// });
 
 // more routes for our API will happen here
 //HTML to allow users to add plant to the database
@@ -46,7 +46,8 @@ app.get('/show.html', function(req, res) {
 });
 //default to show.html
 app.get('/', function(req, res) {
-    res.sendfile('show.html', {root: __dirname })
+    // res.sendfile('show.html', {root: __dirname })
+    res.json({ message: 'hooray! welcome to the heroku deployed app!' });
 });
 
 
